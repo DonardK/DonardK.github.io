5318db75eb4bb9f649f5df597b8fd24190d0b603
DonardK <158263851+DonardK@users.noreply.github.com>
2024-06-09 23:33:44 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
 var navLinks = document.getElementById("navLinks");
-const slides = document.querySelectorAll(".packages .slide");
-let slideIndex = 0;
-let intervalId = null;
-const testimonialSlides = document.querySelectorAll(".testimonials .slide");
-let testimonialIndex = 0;
-let testimonialIntervalId = null;
-
-document.addEventListener("DOMContentLoaded", initializeSliders);
 
 function showMenu(){
     navLinks.style.right = "0";
@@ -32,52 +24,4 @@ function generateRainbowText(element) {
       charElem.innerHTML = text[i];
       element.appendChild(charElem);
     }
-}
-
-function initializeSlider(slideArray, displayClass, nextFunc, prevFunc) {
-  if(slideArray.length > 0){
-    slideArray[0].classList.add(displayClass);
-    if (slideArray === slides) {
-      intervalId = setInterval(nextFunc, 5000);
-    } else {
-      testimonialIntervalId = setInterval(nextFunc, 5000);
-    }
-  }
-}
-
-function showSlide(slideArray, index, displayClass) {
-  if (index >= slideArray.length) {
-    slideIndex = 0;
-  } else if (index < 0) {
-    slideIndex = slideArray.length - 1;
-  } else {
-    slideIndex = index;
-  }
-
-  slideArray.forEach(slide => slide.classList.remove(displayClass));
-  slideArray[slideIndex].classList.add(displayClass);
-}
-
-function prevSlide() {
-  clearInterval(intervalId);
-  showSlide(slides, slideIndex - 1, 'displaySlide');
-  intervalId = setInterval(nextSlide, 5000);
-}
-
-function nextSlide() {
-  clearInterval(intervalId);
-  showSlide(slides, slideIndex + 1, 'displaySlide');
-  intervalId = setInterval(nextSlide, 5000);
-}
-
-function prevTestimonial() {
-  clearInterval(testimonialIntervalId);
-  showSlide(testimonialSlides, testimonialIndex - 1, 'displaySlide');
-  testimonialIntervalId = setInterval(nextTestimonial, 5000);
-}
-
-function nextTestimonial() {
-  clearInterval(testimonialIntervalId);
-  showSlide(testimonialSlides, testimonialIndex + 1, 'displaySlide');
-  testimonialIntervalId = setInterval(nextTestimonial, 5000);
 }
